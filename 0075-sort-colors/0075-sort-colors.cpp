64b78f40a4716d30a9e570019f9a3e8fc871a68c
KSHITIZ <85351591+imkshitiz@users.noreply.github.com>
2023-07-01 13:57:25 +0530

    Time: 0 ms (100.00%), Space: 8.1 MB (83.21%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,22 +1,30 @@
 class Solution {
 public:
-    void sortColors(vector<int>& nums) {
-        int cnt0=0;
-        int cnt1=0;
-        int cnt2=0;
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]==0) cnt0++;
-            else if(nums[i]==1) cnt1++;
-            else cnt2++;
+    void sortColors(vector<int>& nums) { 
+    // optimal solution 
+        
+        int low=0;
+        int mid=0;
+        int high=nums.size()-1;
+        
+    while(mid<=high){
+        if(nums[mid]==0){
+           swap(nums[low],nums[mid]);
+           low++;
+           mid++;
         }
-        for(int i=0;i<cnt0;i++){
-            nums[i]=0;
+        
+        else if(nums[mid]==1){
+           mid++;
         }
-        for(int i=cnt0;i<cnt0+cnt1;i++){
-            nums[i]=1;
-        }
-        for(int i=cnt0+cnt1;i<nums.size();i++){
-                nums[i]=2;
+        
+        else{
+           swap(nums[mid],nums[high]);
+           high--;
+
         }
     }
+        
+    }
+    
 };
