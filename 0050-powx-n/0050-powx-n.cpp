71f7e7aded233b39d5c917b9df873450baf9bc7d
KSHITIZ <85351591+imkshitiz@users.noreply.github.com>
2023-08-23 22:40:05 +0530

    Time: 3 ms (39.31%), Space: 6 MB (27.87%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,6 +1,14 @@
 class Solution {
 public:
-    double myPow(double x, int n) {
-        return pow(x,n);
+    double solve(double x, long n){
+        if(n==0) return 1;
+        
+        if(n<0) return solve(1/x,-n);
+        if(n%2==0) return solve(x*x,n/2);
+        else return x*solve(x*x, (n-1)/2);
+        
     }
+    double myPow(double x, int n) {
+        return solve(x,(long)n);
+        }
 };
