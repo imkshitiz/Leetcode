6e5cc691939042b733573cedbcbfc3275f67a288
KSHITIZ <85351591+imkshitiz@users.noreply.github.com>
2023-09-07 22:26:35 +0530

    Time: 0 ms (100.00%), Space: 7.1 MB (80.26%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,18 +1,20 @@
 class Solution {
 public:
-    vector<vector<int>>ans;
-    int recur(vector<int>&nums,vector<int>&temp,int i){
-        if(i>=nums.size()){
-            ans.push_back(temp);
-            return 0;
+    void recur(int ind,vector<int>&nums, vector<int> &ds,vector<vector<int>> &ans){
+       ans.push_back(ds);
+        for(int i =ind;i<nums.size();i++){
+            if(i!=ind && nums[i]==nums[i-1]) continue;
+            ds.push_back(nums[i]);
+            recur(i+1,nums,ds,ans);
+            ds.pop_back();
         }
-        vector<int>tt=temp;
-        tt.push_back(nums[i]);
-        return max(recur(nums,temp,i+1),recur(nums,tt,i+1));
     }
     vector<vector<int>> subsets(vector<int>& nums) {
-        ans.clear();vector<int>temp;
-        recur(nums,temp,0);
+         vector<vector<int>>ans;
+        vector<int> ds;
+        sort(nums.begin(),nums.end());
+        recur(0,nums,ds,ans);
         return ans;
+    
     }
 };
