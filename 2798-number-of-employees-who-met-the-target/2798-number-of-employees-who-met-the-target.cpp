94698382d2d631210d64fc1107780c41b25d8bab
KSHITIZ <85351591+imkshitiz@users.noreply.github.com>
2024-03-24 00:14:34 +0530

    Time: 2 ms (81.85%), Space: 23.2 MB (61.14%) - LeetHub

---
+++ 2798-number-of-employees-who-met-the-target/2798-number-of-employees-who-met-the-target.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int numberOfEmployeesWhoMetTarget(vector<int>& hours, int target) {
+        
+        int n = hours.size();
+        
+        int ans =0;
+        for(int i = 0;i<n;i++){
+            if(hours[i] < target){
+                continue;
+            }
+            else{
+                ans++;
+            }
+            
+        }
+        return ans;
+    }
+};
